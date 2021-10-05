299ed4d5e0ac303fc9c216b0ef07387d958f8d24
Alessandro Marinelli <alessandro.marinelli@aesys.tech>
2021-10-05 11:45:41 +0200

    clean code

--- src/components/Container/FlightResult/index.tsx
+++ src/components/Container/FlightResult/index.tsx
@@ -15,9 +15,9 @@ const FlightResult: React.FC<frInt> = ({ flightSchedules }) => {
           (x.Flight as FlightType[]).map((y, i) => (
             <>
               <h1 key={i}>{y.Departure.AirportCode}</h1>
-              <h1 key={i}>{y.Departure.ScheduledTimeLocal.DateTime}</h1>
-              <h1 key={i}>{y.Arrival.AirportCode}</h1>
-              <h1 key={i}>{y.Arrival.ScheduledTimeLocal.DateTime}</h1>
+              <h1 key={i + 1}>{y.Departure.ScheduledTimeLocal.DateTime}</h1>
+              <h1 key={i + 2}>{y.Arrival.AirportCode}</h1>
+              <h1 key={i + 3}>{y.Arrival.ScheduledTimeLocal.DateTime}</h1>
               {i === (x.Flight as FlightType[]).length - 1 && (
                 <p style={{ color: "blue" }}>--------</p>
               )}
@@ -26,7 +26,6 @@ const FlightResult: React.FC<frInt> = ({ flightSchedules }) => {
         ) : (
           <>
             <h1>{(x.Flight as FlightType).Departure.AirportCode}</h1>
-            <h1>{(x.Flight as FlightType).Departure.AirportCode}</h1>
             <h1>
               {(x.Flight as FlightType).Departure.ScheduledTimeLocal.DateTime}
             </h1>
@@ -34,6 +33,7 @@ const FlightResult: React.FC<frInt> = ({ flightSchedules }) => {
             <h1>
               {(x.Flight as FlightType).Arrival.ScheduledTimeLocal.DateTime}
             </h1>
+            <p style={{ color: "blue" }}>--------</p>
           </>
         )
       )}
